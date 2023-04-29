a25494b67ceb5135bc4867fa43d3d3a2558ae84c
Сахибгареев Айрат <a.sakhibgareev@bars.group>
2023-04-29 18:29:15 +0300

    builtin

--- block_1/builtin_functions/task_1/implementation.py
+++ block_1/builtin_functions/task_1/implementation.py
@@ -1,2 +1,38 @@
-class Value:
+class MyException(Exception):
     pass
+
+
+class Value:
+    def __init__(self, number):
+        self.number = number
+
+    def __add__(self, other):
+        try:
+            result = self.number + other
+        except:
+            raise MyException
+        else:
+            return result
+
+    def __sub__(self, other):
+        try:
+            result = self.number - other
+        except:
+            raise MyException
+        else:
+            return result
+
+    def __mul__(self, other):
+        try:
+            result = self.number * other
+        except:
+            raise MyException
+        else:
+            return result
+
+    def __truediv__(self, other):
+        if other != 0 and isinstance((self.number, other), int):
+            result = self.number / other
+            return result
+        else:
+            raise MyException
